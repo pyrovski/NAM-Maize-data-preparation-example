604541b42808b2a683a7fc447553bdc3f5d3ed40
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-13 16:04:43 -0700

    split phen.txt by chromosome

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,9 @@
 tail -n +2 NAM_Map_20090730.txt |egrep -o '^[[:digit:]]+'|uniq | xargs -I{} bash -c "egrep '^{}[[:space:]]' NAM_Map_20090730.txt | cut -f1,3- | sed -re 's/m(0)*//' > map.{}.txt"
 #tail -n +2 NAM_Map_20090730.txt | cut -f1,3-|sed -re 's/m(0)*//' > allmap.txt
 
-ls fastphase_chr*.txt | xargs -I{} bash -c "cut -f3,4,12- {} > {}.filtered"
+ls fastphase_chr*.txt | xargs -I{} bash -c "tail -n +2 {} | cut -f3,4,12- > {}.filtered"
 
 ls imputedMarkersGWAS.chr*.082809.txt | xargs -I{} bash -c "tail -n +2 {}|sed -re 's/Z([[:digit:]]+)E([[:digit:]]+)/\1\t\2/' |sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g' > {}.filtered"
+
+#split phen.txt by chromosome
+for i in `tail -n +2 phen.txt |tr -s ' '| tr ' ' '\t'|cut -f1|uniq`; do tail -n +2 phen.txt | tr -s ' ' | tr ' ' '\t'|cut -f1,2,`expr 2 + $i` > phen.$i.txt; done
