162e5505fe7cafa6eacc459a23691a0b354bf080
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-16 16:26:41 -0700

    leave phenotypes in one file

--- prepare.sh
+++ prepare.sh
@@ -9,10 +9,11 @@ ls fastphase_chr*.txt | xargs -I{} bash -c "tail -n +2 {} | cut -f3,4,12- > {}.f
 # format: MO###; the rest is the same as the GWAS markers
 ls imputedIBMMarkers*.txt | xargs -I{} bash -c "tail -n +2 {} | sed -re 's/^MO(0)*/17\t/' > {}.filtered"
 
-ls imputedMarkersGWAS.chr*.082809.txt | xargs -I{} bash -c "tail -n +2 {}|sed -re 's/Z([[:digit:]]+)E([[:digit:]]+)/\1\t\2/' |sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g' > {}.filtered"
+ls imputedMarkersGWAS.chr*.082809.txt | xargs -I{} bash -c "tail -n +2 {}|sed -re 's/Z([[:digit:]]+)E([[:digit:]]+)/\1\t\2/' \
+|sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g' > {}.filtered"
 
 # merge GWAS and IBM markers
 ls imputed*chr*.txt.filtered |egrep -o 'chr[[:digit:]]+'|sort | uniq | xargs -I{} bash -c "cat imputed*{}.*.filtered > imputedMarkers.{}.merged"
 
-#split phen.txt by chromosome
-for i in `tail -n +2 phen.txt |tr -s ' '| tr ' ' '\t'|cut -f1|uniq`; do tail -n +2 phen.txt | tr -s ' ' | tr ' ' '\t'|cut -f1,2,`expr 2 + $i` > phen.$i.txt; done
+# filter residuals
+tail -n +2 residuals_distancebased_for_asi.txt|sed -re 's/Z([[:digit:]]+)E([[:digit:]]+)/\1\t\2/' | sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g'| sed -re 's/^MO(0)*/17\t/' > residuals_distancebased_for_asi.txt.filtered
