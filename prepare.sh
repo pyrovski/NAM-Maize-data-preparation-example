e2e22ec696f0ac6c97f9b8f818dd47e176f0b9ae
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-05-02 13:01:19 -0500

    speed up prepare.sh a bit

--- prepare.sh
+++ prepare.sh
@@ -17,8 +17,15 @@ ls imputedMarkersGWAS.chr*.082809.txt | xargs -I{} bash -c "tail -n +2 {}|sed -r
 # merge GWAS and IBM markers
 # the GWAS and IBM markers are independently sorted, and need to be merged.
 # sort -m
-ls imputed*chr*.txt.filtered |egrep -o 'chr[[:digit:]]+'|sort | uniq | xargs -I{} bash -c "sort -m imputed*{}.*.filtered | sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g' > imputedMarkers.{}.merged"
+ls imputed*chr*.txt.filtered |egrep -o 'chr[[:digit:]]+'|sort|uniq | xargs -I{} bash -c "sort -m imputed*{}.*.filtered > imputedMarkers.{}.merged"
+#| sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g'
 
 # filter residuals
 # the sorting could be faster; MO and Z sections are already sorted
-tail -n +2 residuals_distancebased_for_asi.txt|sed -re 's/Z([[:digit:]]+)E([[:digit:]]+)/\1\t\2/' | sed -re 's/^MO(0)*/17\t/' | sort -g +0 -2 | sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g' > residuals_distancebased_for_asi.txt.filtered
+tail -n +2 residuals_distancebased_for_asi.txt | grep -n Z018|cut -d':' -f1|head -1 > .first18
+(head -n `cat .first18 ` residuals_distancebased_for_asi.txt; \
+grep MO residuals_distancebased_for_asi.txt|sed -re 's/^MO(0)*/17\t/';  \
+tail -n +2 residuals_distancebased_for_asi.txt | tail -n +`cat .first18` | grep -v MO ) | \
+sed -re 's/Z([[:digit:]]+)E([[:digit:]]+)/\1\t\2/' | sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g' > residuals_distancebased_for_asi.txt.filtered
+
+#tail -n +2 residuals_distancebased_for_asi.txt|sed -re 's/Z([[:digit:]]+)E([[:digit:]]+)/\1\t\2/' | sed -re 's/^MO(0)*/17\t/' | sort -g +0 -2 | sed -re 's/([[:space:]]|^)(0)*([1-9.]+)/\1\3/g' > residuals_distancebased_for_asi.txt.filtered
